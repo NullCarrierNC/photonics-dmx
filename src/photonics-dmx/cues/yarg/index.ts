31e6c1bc65cbb99ba63544c2cdd2dd136df34714
Null Carrier <nc@photonics.rocks>
2025-04-18 13:50:46 -0400

    Registering the group
    Need to fix the FE selector when changing groups and cues

--- src/photonics-dmx/cues/yarg/index.ts
+++ src/photonics-dmx/cues/yarg/index.ts
@@ -1,5 +1,6 @@
 // Import the cue registry initialization to ensure it runs
 import './groups/default';
+import './groups/coolWarmAlt1';
 
 // Export the registry for use by other modules
 export { CueRegistry } from '../CueRegistry'; 
