af8fb49ebb98ed6e7a38eecc3cc20d2217e48426
Null Carrier <nc@photonics.rocks>
2025-06-04 16:56:48 -0400

    WIP: Warm/Cool Alt set 2
    Introducing some new state tracking capabilities, currently doesn't reset when the cue ends. Need to fix that.

--- src/photonics-dmx/cues/yarg/groups/coolWarmAlt2.ts
+++ src/photonics-dmx/cues/yarg/groups/coolWarmAlt2.ts
@@ -1,23 +1,19 @@
 import { CueRegistry } from '../../CueRegistry';
 import { CueType } from '../../cueTypes';
 import { ICueGroup } from '../../interfaces/ICueGroup';
-import { CoolAutomaticCue } from '../coolWarmAlt1/CoolAutomaticCue';
-import { CoolManualCue } from '../coolWarmAlt1/CoolManualCue';
-import { WarmAutomaticCue } from '../coolWarmAlt1/WarmAutomaticCue';
-import { WarmManualCue } from '../coolWarmAlt1/WarmManualCue';
-
+import { CoolAutomaticCue } from '../coolWarmAlt2/CoolAutomaticCue';
 
 /**
  * Create and register the cool/warm alternative 1 cue group.
  */
 const group: ICueGroup = {
-  name: 'Cool/Warm Alternative 1',
+  name: 'Cool/Warm Alternative 2',
   description: 'Alt versions of the cool and warm cues, less frenetic than the defaults.',
   cues: new Map([
     [CueType.Cool_Automatic, new CoolAutomaticCue()],
-    [CueType.Cool_Manual, new CoolManualCue()],
-    [CueType.Warm_Automatic, new WarmAutomaticCue()],
-    [CueType.Warm_Manual, new WarmManualCue()],
+  //  [CueType.Cool_Manual, new CoolManualCue()],
+   // [CueType.Warm_Automatic, new WarmAutomaticCue()],
+   // [CueType.Warm_Manual, new WarmManualCue()],
   ]),
 };
 
