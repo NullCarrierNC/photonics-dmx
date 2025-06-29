162570e1813932b9220afd6a0f6d5454cbefb670
Null Carrier <nc@photonics.rocks>
2025-06-29 15:09:18 -0400

    WIP: Searchlights, update to preview logic
    Preview selector has a remaining issue if the effect list only contains one effect we can't change it in order to set it active.

--- src/photonics-dmx/cues/yarg/index.ts
+++ src/photonics-dmx/cues/yarg/index.ts
@@ -2,6 +2,7 @@
 import './groups/default';
 import './groups/coolWarmAlt1';
 import './groups/coolWarmAlt2';
+import './groups/commonAlt1';
 
 // Export the registry for use by other modules
 export { CueRegistry } from '../CueRegistry'; 
