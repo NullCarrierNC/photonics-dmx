3674627953e387d80b989029f4b1533f8e7b60fd
Null Carrier <nc@photonics.rocks>
2025-09-01 16:06:41 -0400

    Tweaks to silhouette spotlight and intro cues
    Spotlights should react to drum-red

--- src/photonics-dmx/cues/yarg/handlers/default/SilhouettesSpotlightCue.ts
+++ src/photonics-dmx/cues/yarg/handlers/default/SilhouettesSpotlightCue.ts
@@ -4,27 +4,56 @@ import { DmxLightManager } from '../../../../controllers/DmxLightManager';
 import { ICue, CueStyle } from '../../../interfaces/ICue';
 import { getColor } from '../../../../helpers/dmxHelpers';
 import { getEffectSingleColor } from '../../../../effects/effectSingleColor';
+import { getEffectFlashColor } from '../../../../effects';
 
 export class SilhouettesSpotlightCue implements ICue {
   id = 'default-silhouettes-spotlight';
   cueId = CueType.Silhouettes_Spotlight;
-  description = 'Solid low-intensity blue color on all lights (front and back)';
+  description = 'Flash blue on back lights (or front if no back lights) triggered by drum-red';
   style = CueStyle.Primary;
+  private isFirstExecution = true;
 
   async execute(_parameters: CueData, sequencer: ILightingController, lightManager: DmxLightManager): Promise<void> {
-    const low = getColor('blue', 'low');
-
-    const singleColor = getEffectSingleColor({
-      waitFor: 'none',
-      forTime: 0,
-      color: low,
-      duration: 100,
-      waitUntil: 'none',
-      untilTime: 0,
-      lights: lightManager.getLights(['front', 'back'], 'third-2'),
-      layer: 0,
-    });
-
-    sequencer.setEffect('silhouettes_spot', singleColor);
+      // Get back lights first, fall back to front lights if no back lights
+      const backLights = lightManager.getLights(['back'], 'all');
+      const targetLights = backLights.length > 0 ? backLights : lightManager.getLights(['front'], 'all');
+      const blue = getColor('blue', 'medium');
+  
+      // Create drum-red triggered flash effect
+      const flashEffect = getEffectFlashColor({
+        lights: targetLights,
+        color: blue,
+        startTrigger: 'drum-red',
+        holdTime: 200,
+        durationIn: 0, 
+        durationOut: 0,
+        layer: 1,
+      });
+  
+      const solidEffect = getEffectSingleColor({
+        lights: lightManager.getLights(['front', 'back'], 'all'),
+        color: getColor('transparent', 'medium'),
+        duration: 10,
+        layer: 0,
+      });
+           // Apply the effects
+       if (this.isFirstExecution) {
+         // First time: use setEffect to clear any existing effects and start fresh
+         await sequencer.setEffect('silhouettes-spotlight-off', solidEffect);
+         await sequencer.addEffect('silhouettes-spotlight-blue', flashEffect);
+         this.isFirstExecution = false;
+       } else {
+         // Repeat call: use addEffect to add to existing effects
+         sequencer.addEffect('silhouettes-spotlight-off', solidEffect);
+         sequencer.addEffect('silhouettes-spotlight-blue', flashEffect);
+       }
   }
+
+
+  onStop(): void {
+    // Reset the first execution flag so next time this cue runs it will use setEffect
+    this.isFirstExecution = true;
+  }
+
+
 } 
--- src/photonics-dmx/cues/yarg/handlers/stagekit/StageKitSilhouettesSpotlightCue.ts
+++ src/photonics-dmx/cues/yarg/handlers/stagekit/StageKitSilhouettesSpotlightCue.ts
@@ -2,22 +2,60 @@ import { ICue, CueStyle } from '../../../interfaces/ICue';
 import { CueData, CueType } from '../../../cueTypes';
 import { ILightingController } from '../../../../controllers/sequencer/interfaces';
 import { DmxLightManager } from '../../../../controllers/DmxLightManager';
+import { getEffectFlashColor } from '../../../../effects/effectFlashColor';
+import { getColor } from '../../../../helpers/dmxHelpers';
+import { getEffectSingleColor } from '../../../../effects';
 
 /**
- * StageKit Silhouettes Spotlight Cue - Silhouette spotlight with vocal interaction
+ * StageKit Silhouettes Spotlight Cue - Beat-synchronized flash on front and back lights
  */
 export class StageKitSilhouettesSpotlightCue implements ICue {
   id = 'stagekit-silhouettesspotlight';
   cueId = CueType.Silhouettes_Spotlight;
-  description = 'YALCY currently blacks out on this cue, so we do too.';
+  description = 'Flash blue on front and back lights (third-2) on drum-red';
   style = CueStyle.Primary;
+  private isFirstExecution = true;
 
-  async execute(_cueData: CueData, sequencer: ILightingController, _lightManager: DmxLightManager): Promise<void> {
-    sequencer.blackout(0);
+  async execute(_cueData: CueData, sequencer: ILightingController, lightManager: DmxLightManager): Promise<void> {
+    // Get front and back lights
+    const frontLights = lightManager.getLights(['front'], 'third-2');
+    const backLights = lightManager.getLights(['back'], 'third-2');
+    const allLights = [...frontLights, ...backLights];
+    const blue = getColor('blue', 'medium');
+
+    // Create beat-synchronized flash effect
+    const flashEffect = getEffectFlashColor({
+      lights: allLights,
+      color: blue,
+      startTrigger: 'drum-red',
+      holdTime: 200,
+      durationIn: 0, 
+      durationOut: 0,
+      layer: 1,
+    });
+
+    const solidEffect = getEffectSingleColor({
+      lights:  lightManager.getLights(['front', 'back'], 'all'),
+      color: getColor('transparent', 'medium'),
+      duration: 10,
+      layer: 0,
+    });
+         // Apply the effects
+     if (this.isFirstExecution) {
+       // First time: use setEffect to clear any existing effects and start fresh
+       await sequencer.setEffect('silhouettes-spotlight-off', solidEffect);
+       await sequencer.addEffect('silhouettes-spotlight-blue', flashEffect);
+       this.isFirstExecution = false;
+     } else {
+       // Repeat call: use addEffect to add to existing effects
+       sequencer.addEffect('silhouettes-spotlight-off', solidEffect);
+       sequencer.addEffect('silhouettes-spotlight-blue', flashEffect);
+     }
   }
 
   onStop(): void {
-    
+    // Reset the first execution flag so next time this cue runs it will use setEffect
+    this.isFirstExecution = true;
   }
 
   onPause(): void {
