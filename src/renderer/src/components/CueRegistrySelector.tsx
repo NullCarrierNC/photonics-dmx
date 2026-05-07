646fa1dd71dcdfb6b581d0acb0e142b6915f7fa0
Null Carrier <nc@photonics.rocks>
2026-05-06 21:18:59 -0400

    Fix alpha sorting

--- src/renderer/src/components/CueRegistrySelector.tsx
+++ src/renderer/src/components/CueRegistrySelector.tsx
@@ -64,17 +64,21 @@ const CueRegistrySelector: React.FC<CueRegistrySelectorProps> = ({
         (g: CueGroup) => enabledGroupIds.includes(g.id) && g.cueTypes.length > 0,
       )
 
-      log.info(`Enabled groups:`, enabledGroups)
-      setGroups(enabledGroups)
+      const sortedGroups = [...enabledGroups].sort((a, b) =>
+        a.name.localeCompare(b.name, undefined, { sensitivity: 'base' }),
+      )
+
+      log.info(`Enabled groups:`, sortedGroups)
+      setGroups(sortedGroups)
 
       if (selectedGroup === '') {
-        if (enabledGroups.length > 0 && isInitialMount.current) {
-          const firstGroup = enabledGroups[0]
+        if (sortedGroups.length > 0 && isInitialMount.current) {
+          const firstGroup = sortedGroups[0]
           setSelectedGroup(firstGroup.id)
           handleGroupChangeCallback(firstGroup.id)
           isInitialMount.current = false
         }
-      } else if (isInitialMount.current && enabledGroups.length > 0) {
+      } else if (isInitialMount.current && sortedGroups.length > 0) {
         handleGroupChangeCallback(selectedGroup)
         isInitialMount.current = false
       }
